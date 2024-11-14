03a18e018e761b9abbaa38bea2ac44bfe200b104
zackmorgs <zackmorgenthaler@gmail.com>
2024-11-14 09:28:47 -0600

    intial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: [
+        './src/EcomComponents/**/*.razor',
+        './src/EcomComponents/**/*.cshtml',
+        './src/EcomComponents/**/*.html',
+        './src/EComServer/**/*.razor',
+        './src/EComServer/**/*.cshtml',
+        './src/EComServer/**/*.html'
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
