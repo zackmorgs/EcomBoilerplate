0e8dd2e99c48c87d77f7fa352b89bb18712e7fc7
zackmorgs <zackmorgenthaler@gmail.com>
2024-11-15 08:09:48 -0600

    scaffolded out Identity again because the cli failed to give me all the pages

--- src/EcomServer/Migrations/20241115140555_Migration2.Designer.cs
+++ src/EcomServer/Migrations/20241115140555_Migration2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EcomServer.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EcomServer.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241115140555_Migration2")]
+    partial class Migration2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.0");
